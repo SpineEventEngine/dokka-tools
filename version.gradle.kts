edca1588394db86623b9c33904b16fb599c632c2
alexander-yevsyukov <alexander.yevsyukov@teamdev.com>
2025-02-05 19:11:47 +0000

    Bump version -> `2.0.0-SNAPSHOT.6`

--- version.gradle.kts
+++ version.gradle.kts
@@ -1,11 +1,11 @@
 /*
- * Copyright 2022, TeamDev. All rights reserved.
+ * Copyright 2025, TeamDev. All rights reserved.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- * http://www.apache.org/licenses/LICENSE-2.0
+ * https://www.apache.org/licenses/LICENSE-2.0
  *
  * Redistribution and use in source and/or binary forms, with or without
  * modification, must retain the above copyright notice and the following
@@ -27,6 +27,6 @@
 /**
  * The version of this library for publishing.
  *
- * For dependencies on Spine modules please see [io.spine.internal.dependency.Spine].
+ * For dependencies on Spine modules please see [io.spine.dependency.local.Spine].
  */
-val versionToPublish: String by extra("2.0.0-SNAPSHOT.5")
+val versionToPublish: String by extra("2.0.0-SNAPSHOT.6")
